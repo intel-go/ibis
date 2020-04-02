f4dc0a013a973afafab580dc21bf3f12149ef684
Vasilij Litvinov <vasilij.n.litvinov@intel.com>
2020-04-03 00:41:04 +0300

    Change con.create_table_from_csv to use CREATE DATAFRAME
    Also add ability to use SKIP_ROWS, DELIMITER and HEADER options
    
    Warning: untested

--- ibis/omniscidb/client.py
+++ ibis/omniscidb/client.py
@@ -1061,14 +1061,14 @@ class OmniSciDBClient(SQLClient):
         self.set_database(_database)
 
     def create_table_from_csv(
-        self, table_name, csv_file, schema, database=None, max_rows=None, fragment_size=None
+        self, table_name, csv_file, schema, database=None, max_rows=None, fragment_size=None, skip_rows=None, delimiter=None, header=None
     ):
         _database = self.db_name
         self.set_database(database)
 
         if schema is not None:
             statement = ddl.CreateTableFromCsv(
-                table_name, schema, csv_file, database=database, max_rows=max_rows, fragment_size=fragment_size
+                table_name, schema, csv_file, database=database, max_rows=max_rows, fragment_size=fragment_size, skip_rows=skip_rows, delimiter=delimiter, header=header
             )
         else:
             raise com.IbisError('Must pass expr or schema')
